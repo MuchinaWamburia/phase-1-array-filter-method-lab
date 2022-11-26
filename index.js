de67350e694ede7beffc84c72fbd3d173efa6a3c
MuchinaWamburia <lucymuchina20@gmail.com>
2022-11-26 15:52:40 +0300

    competed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === argument.toLowerCase() })
+  }
+  
+function fuzzyMatch(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, argument.length) === argument.toLowerCase() })
+  }
+  
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
